008c7594d6fa2c16487d51daea8cb42551977950
stivenAigle <aiglestiven@gmail.com>
2018-10-16 13:29:52 +0200

    add vao.h

---
+++ inc/glish3/Vao.h
@@ -0,0 +1,28 @@
+//
+// Created by stiven on 10/16/18.
+//
+
+#ifndef GLISH3_VAP_H
+#define GLISH3_VAP_H
+
+#include <glish3/gl_glew.hpp>
+namespace glish3{
+
+    class Vao{
+        GLuint vao = 0;
+
+
+    public:
+        Vao();
+        Vao(Vao const &) = delete;
+        Vao& operator=(Vao const &) = delete;
+
+        Vao(Vao && vao);
+        Vao&operator=(Vao && vao);
+
+        operator bool() const;
+        operator GLuint();
+        ~Vao();
+    };
+}
+#endif //GLISH3_VAP_H
