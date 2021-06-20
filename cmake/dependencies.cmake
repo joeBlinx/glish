58a21be1c44b0f6a114c98f855233537124df380
joeBlinx <aiglestiven@gmail.com>
2021-06-20 20:40:22 +0200

    huge refacto on cmake, use target everywhere, remove unnecessary comments

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,13 @@
+# Download automatically, you can also just copy the conan.cmake file
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
+            "${CMAKE_BINARY_DIR}/conan.cmake"
+            TLS_VERIFY ON)
+endif()
+
+include(${CMAKE_BINARY_DIR}/conan.cmake)
+conan_cmake_run(REQUIRES glm/0.9.9.8
+        stb/20200203
+        BASIC_SETUP CMAKE_TARGETS
+        BUILD missing)
