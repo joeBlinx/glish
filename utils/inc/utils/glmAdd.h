002e6a3b04af052ea655146a2735f74c69207e4b
joeBlinx <aiglestiven@gmail.com>
2021-06-20 20:02:08 +0200

    add badge for build

--- utils/inc/utils/glmAdd.h
+++ utils/inc/utils/glmAdd.h
@@ -5,6 +5,6 @@
 #ifndef GLENGINE_GLMADD_H
 #define GLENGINE_GLMADD_H
 
-#include <glm/glm.hpp>
+#include "glm/glm.hpp"
 extern bool operator<=(glm::vec3 const & a, glm::vec3 const & b);
 #endif //GLENGINE_GLMADD_H
